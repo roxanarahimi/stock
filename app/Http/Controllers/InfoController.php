387b90d1457bcdce3afd5d1b6bf5ef8b9d0f01d4
Roxana <ms.roxanarahimi@gmail.com>
2024-11-23 16:33:44 +0330

    ss

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -60,7 +60,7 @@ public function index2(Request $request)
     {
         $dat2 = DB::connection('sqlsrv')->table('LGS3.Part')->orderBy('PartID')
             ->select('PartID','PartName')
-           -> where('State',1)
+           ->where('State',1)
             ->get()->count();
         return $dat2;
     }
