ac698735ba12b6a581e8dc8aef71105d3c0a442a
Roxana <ms.roxanarahimi@gmail.com>
2024-11-24 14:14:50 +0330

    ss

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -129,10 +129,11 @@ public function index2(Request $request)
         try {
             Info::query()->truncate();
             $dat = DB::connection('sqlsrv')->table('DBO.MS_VWStorePartFactorRemainQuantity')
-                ->get();
+             ->take(50)->get();
+            return $dat;
             foreach ($dat as $item) {
                 $d = Info::where('StoreCode', $item->StoreCode)->where('PartCode', $item->PartCode)
-                    ->where('ّFactor', $item->PartCode)->first();
+                    ->where('ّFactor', $item->ّFactor)->first();
                 if ($d && (integer)$item->Quantity != (integer)$d->Quantity) {
                     $d->update([
                         'Quantity' => (integer)$item->Quantity
